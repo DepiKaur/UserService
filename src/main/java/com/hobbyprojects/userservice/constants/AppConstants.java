d844471489f650c7c9389d6b5042faa56cf86b9d
Depinder Kaur <depinder.kaur@iths.se>
2024-04-02 10:45:54 +0200

    Created a new class AppConstants that contains the constants. for ex. the regular expression of the email.

---
+++ src/main/java/com/hobbyprojects/userservice/constants/AppConstants.java
@@ -0,0 +1,17 @@
+package com.hobbyprojects.userservice.constants;
+
+/**
+ * @author Depinder Kaur
+ * @version 0.1
+ * <h2>AppConstants</h2>
+ * @date 2024-04-02
+ */
+public class AppConstants {
+
+    // private constructor to avoid/prevent instantiation of this class.
+    private AppConstants() {
+
+    }
+
+    public static final String EMAIL_REGEX = "^(.+)@(.+)$";
+}
