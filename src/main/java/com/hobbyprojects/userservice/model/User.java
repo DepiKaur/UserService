48ca982c036adcb96e662edb3cf6552ffffa06da
Depinder Kaur <depinder.kaur@iths.se>
2024-04-02 10:38:46 +0200

    Optimized imports

--- src/main/java/com/hobbyprojects/userservice/model/User.java
+++ src/main/java/com/hobbyprojects/userservice/model/User.java
@@ -10,7 +10,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 
 import java.util.Date;
 
