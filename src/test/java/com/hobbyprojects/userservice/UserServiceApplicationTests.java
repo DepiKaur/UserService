9d92855cf919567aadb3918a7cf916248c00da4b
Depinder Kaur <depinder.kaur@iths.se>
2024-04-24 23:04:12 +0200

    Added tests to test email

--- src/test/java/com/hobbyprojects/userservice/UserServiceApplicationTests.java
+++ src/test/java/com/hobbyprojects/userservice/UserServiceApplicationTests.java
@@ -1,13 +1,30 @@
 package com.hobbyprojects.userservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 @SpringBootTest
 class UserServiceApplicationTests {
 
+    @Autowired
+    private JavaMailSender emailSender;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void testSendEmail(){
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo("martha@test.com");
+        message.setSubject("Order Confirmation, Order No : 12345");
+        message.setText("Thanks for placing an order with us.");
+        emailSender.send(message);
+
     }
 
 }
